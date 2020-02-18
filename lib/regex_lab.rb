8904848b8590ac828368b8e2d65c11914082b2be
Alex Romo <ayromo08@gmail.com>
2020-02-18 23:23:25 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/[0-9]\S*{10}\b/) != nil
+  phone.match(/[0-9]\d*{10}/) != nil
 end
